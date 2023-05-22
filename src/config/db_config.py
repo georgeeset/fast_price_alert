f11528c72b8bbe452622c7704db14c0e48355b66
George Eset <georgeperfect4u@gmail.com>
2023-05-22 02:03:07 +0100

    setup ongoing

--- src/config/db_config.py
+++ src/config/db_config.py
@@ -1,1 +1,15 @@
 """Configure Mysql database"""
+from sqlalchemy import create_engine
+
+
+"""Instantiate a DBStorage object"""
+HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+HBNB_ENV = getenv('HBNB_ENV')
+engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                              format(HBNB_MYSQL_USER,
+                                     HBNB_MYSQL_PWD,
+                                    HBNB_MYSQL_HOST,
+                                    HBNB_MYSQL_DB))
--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,5 +1,5 @@
 """module contains data structure for user"""
 from sqlalchemy import column
 
+
 class User_model():
-    
